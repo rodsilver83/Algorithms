40a3db55ba5b3b394159d073bf65008020d1884c
Rodrigo Soria <rodsilver83@hotmail.com>
2019-01-21 04:28:34 +0000

    more

--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -5,11 +5,11 @@ const mergeSort = S.mergeSort;
 const merge = S.merge;
 
 function getArray() {
-  return [100, -40, 500, -124, 0, 21, 7];
+  return [100, -40, 500, -124, 0, 21, 700];
 }
 
 function getSortedArray() {
-  return [-124, -40, 0, 7, 21, 100, 500];
+  return [-124, -40, 0, 21, 100, 500, 700];
 }
 
 describe('Bubble sort', () => {
